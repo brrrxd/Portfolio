a1f996ec9d0259995ab8de8bca61d9aeb36210f1
Matias Kallio <matias.kallio@edu.salpaus.fi>
2020-11-23 09:53:45 +0000

    20201123

--- script3.js
+++ script3.js
@@ -6,6 +6,7 @@ const virhe = document.querySelector('.virhe');
 const tiedot = document.querySelector('#tiedot');
 const ree = document.querySelector('#ree');
 const ree2 = document.querySelector('#ree2');
+const terveiset = document.querySelector('.terveiset');
 
 lahetanappi.addEventListener('click', e =>{
   e.preventDefault();
@@ -30,4 +31,33 @@ lahetanappi.addEventListener('click', e =>{
     pakemail.value= '';
   }
 });
+}
+window.onload=function(){
+const emailnappi = document.querySelector('.lahetanappi');
+
+emailnappi.addEventListener('click', e =>{
+  e.preventDefault();
+  sendJSON();
+});
+
+function sendJSON(){
+  let xhr = new XMLHttpRequest();
+  let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
+
+  xhr.open("POST", url, true);
+
+  xhr.setRequestHeader("Content-type", "application/json");
+
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200)
+    console.log("Valmis, yhteys toimii");
+  };
+  var data =JSON.stringify({
+    "EmailMsg": pakemail, //kirjoittaa sähköpostin sisällön
+    "EmailAddress": terveiset, //viestin kirjoittajan sähköpostin
+    "EmailTo": "matias.kallio@edu.salpaus.fi", //oma sähköpostin
+    "EmailName": paknimi //Nimi kentän sisältö
+  });
+  xhr.send(data);
+};
 }
